9a2a141942d8373f7eb646e349b6a39a3f49c5b8
root <root@iblanstwtstv01t.weizmann.ac.il>
2021-07-15 12:05:43 +0300

    Upload registerrh8.sh

--- scripts/registerrh8.sh
+++ scripts/registerrh8.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 
 # This script registers a system into Satellite server
@@ -7,4 +7,8 @@ curl -O http://iblstlv01.weizmann.ac.il/pub/bootstrap.py
 
 chmod +x bootstrap.py
 
-/usr/libexec/platform-python bootstrap.py --login=mordan --server iblstlv01.weizmann.ac.il --location="Weizmann Lan" --organization="Weizmann Institute of Science" --hostgroup="Dev_RHEL8_VC" --activationkey=Dev --force --download-method https --skip foreman
+os=$(cat /etc/redhat-release)
+osver_pos=$(cat /etc/redhat-release | grep -aob release | cut -d: -f1)
+release=${os:osver_pos+8:3}
+
+/usr/libexec/platform-python bootstrap.py --login=mordan --server iblstlv01.weizmann.ac.il --release=$release --location="Weizmann Lan" --organization="Weizmann_Institute_of_Science" --hostgroup="Dev_RHEL8_VC" --activationkey=Dev --force --download-method https --skip foreman
