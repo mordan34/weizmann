09d0832fef8b4929e01e473aa530e1551264ec02
root <root@iblstlv01.weizmann.ac.il>
2021-08-25 15:53:54 +0300

    Satellite

---
+++ scripts/addhost.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+server="iblstlv01.weizmann.ac.il"
+
+hammer  --server https://$server --username admin host create --name $1 --puppet-environment-id 3 --architecture-id 1 --domain-id 1  --puppet-proxy-id 1 --operatingsystem-id 3 --partition-table-id 7 --location-id 2 --organization-id 1 --medium-id 12 --mac "$(facter macaddress)" --build 1 --hostgroup "Dev_RHEL8_VC" &
+
+pid=$!
+wait $pid
+name="$1"'.weizmann.ac.il'
+
+hammer host start --name $name
