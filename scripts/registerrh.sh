ddb484708236c9c0996d863d5ab89abb9df4f22a
Mor Danino <mdanino27@gmail.com>
2021-09-01 15:06:41 +0300

    laptop

--- scripts/registerrh.sh
+++ scripts/registerrh.sh
@@ -3,9 +3,7 @@
 
 # This script registers a system into Satellite server
 
-curl -O https://iblstlv01.weizmann.ac.il/pub/bootstrap.py
 
-chmod +x bootstrap.py
 
 os=$(cat /etc/redhat-release)
 osver_pos=$(cat /etc/redhat-release | grep -aob release | cut -d: -f1)
@@ -21,4 +19,6 @@ yum -y install katello-host-tools
 yum -y install katello-host-tools-tracer
 yum -y install katello-agent
 
+#curl -O https://iblstlv01.weizmann.ac.il/pub/bootstrap.py
+#chmod +x bootstrap.py
 #/usr/libexec/platform-python bootstrap.py --login=admin --server iblstlv01.weizmann.ac.il --release=$release --location="Weizmann Lan" --organization="Weizmann_Institute_of_Science" --hostgroup="VXRail_VC" --activationkey=Dev --force --download-method https --skip 'foreman' --install-katello-agent
