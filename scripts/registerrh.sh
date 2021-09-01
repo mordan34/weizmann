c39c55e8c0dbfa779c098198053811c55ce7e0df
Mor Danino <mdanino27@gmail.com>
2021-09-01 14:50:05 +0300

    laptop

--- scripts/registerrh.sh
+++ scripts/registerrh.sh
@@ -11,4 +11,13 @@ os=$(cat /etc/redhat-release)
 osver_pos=$(cat /etc/redhat-release | grep -aob release | cut -d: -f1)
 release=${os:osver_pos+8:3}
 
-/usr/libexec/platform-python bootstrap.py --login=admin --server iblstlv01.weizmann.ac.il --release=$release --location="Weizmann Lan" --organization="Weizmann_Institute_of_Science" --hostgroup="VXRail_VC" --activationkey=Dev --force --download-method https --skip 'foreman' --install-katello-agent
+# Download and install Satellite's CA certificate
+curl --insecure --output katello-ca-consumer-latest.noarch.rpm https://iblstlv01.weizmann.ac.il/pub/katello-ca-consumer-latest.noarch.rpm
+yum localinstall katello-ca-consumer-latest.noarch.rpm 
+
+subscription-manager register --org="Weizmann_Institute_of_Science" --activationkey="Dev"
+yum -y install katello-host-tools
+yum -y install katello-host-tools-tracer
+yum -y install katello-agent
+
+#/usr/libexec/platform-python bootstrap.py --login=admin --server iblstlv01.weizmann.ac.il --release=$release --location="Weizmann Lan" --organization="Weizmann_Institute_of_Science" --hostgroup="VXRail_VC" --activationkey=Dev --force --download-method https --skip 'foreman' --install-katello-agent
