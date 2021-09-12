d18299a706b50da84ca277522d72b39a29a0c84a
root <root@iblstlv01.weizmann.ac.il>
2021-09-12 10:24:02 +0300

    Satellite

--- scripts/createhost.sh
+++ scripts/createhost.sh
@@ -3,7 +3,7 @@
 server="iblstlv01.weizmann.ac.il"
 
 # Create host from image on the VC
-hammer  --server https://$server --username admin host create --name $1 --hostgroup "VXRail_VC" --compute-profile-id 2 --image "RHEL-8-64-LAN" --partition-table-id 109 --medium-id 12 --puppet-environment-id 1 --architecture-id 1 --domain-id 1  --puppet-proxy-id 1 --location-id 2 --organization-id 1 --mac "$(facter macaddress)" --build 1 &
+hammer  --server https://$server --username admin host create --name $1 --hostgroup "VXRail_VC" --provision-method 'image' --enabled true --managed true --compute-profile-id 2 --image "RHEL-8-64-LAN" --puppet-environment-id 3 --architecture-id 1 --domain-id 1  --puppet-proxy-id 1 --location-id 2 --organization-id 1 --build 1 --interface "managed=true,primary=true,provision=true,compute_type=VirtualVmxnet3,compute_network=VLAN_3665_Application" &
 
 # Waiting for the host creation to finish
 pid=$!
